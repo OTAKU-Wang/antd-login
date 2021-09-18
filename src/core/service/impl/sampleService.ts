b3e504c1e56d56faee083529e10d179d18c2b998
wangmaoshuai <eric414664593@gmail.com>
2021-09-18 11:49:15 +0800

    update

---
+++ src/core/service/impl/sampleService.ts
@@ -0,0 +1,18 @@
+import { SignUpService, SignInService } from "../../service";
+import { Credential, Email, User } from "../../entities";
+// TODO: 还没完全实现，需要添加后端请求
+export class SampleService implements SignInService, SignUpService {
+    async signInWithCredential(credential: Credential): Promise<User> {
+        return Promise.resolve(
+            new User("Sample Service", "User", "sampleservice@email.com"),
+        );
+    }
+
+    async signUpUser(user: User, credential: Credential): Promise<User> {
+        return Promise.resolve(user);
+    }
+
+    async verifyExistingEmail(email: Email): Promise<boolean> {
+        return Promise.resolve(false);
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
 import reportWebVitals from './reportWebVitals';
+import SignInComponent from './web/components/SignInComponent';
+import SignInHeaderComponent from './web/components/SignInHeaderComponent';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SignInHeaderComponent />
+    <SignInComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +16,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
